5f3ab05edba3b2c07844e01296535cfa6e972703
vladogrcic <vladogrcic1993@gmail.com>
2019-04-20 21:25:56 +0200

    Create uploadAdapter.js

---
+++ src/plugins/uploadAdapter.js
@@ -0,0 +1,88 @@
+export default class UploadAdapter extends Plugin{
+    constructor( loader ) {
+        // CKEditor 5's FileLoader instance.
+        this.loader = loader;
+
+        // URL where to send files.
+		this.url = 'https://example.com/image/upload/path';
+		// return this.editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader ) => {
+		// 	return this( loader );
+		// };
+    }
+	// init(){
+	// 	editor.ui.componentFactory.add( 'imageEmbed', locale => {
+	// 		const view = new ButtonView( locale );
+
+    //         view.set( {
+    //             label: 'Insert image',
+    //             icon: imageIcon,
+    //             tooltip: true
+	// 		} );
+	// 		return view;
+	// 	});
+	// }
+    // Starts the upload process.
+    upload() {
+        return new Promise( ( resolve, reject ) => {
+            this._initRequest();
+            this._initListeners( resolve, reject );
+            this._sendRequest();
+        } );
+    }
+
+    // Aborts the upload process.
+    abort() {
+        if ( this.xhr ) {
+            this.xhr.abort();
+        }
+    }
+
+    // Example implementation using XMLHttpRequest.
+    _initRequest() {
+        const xhr = this.xhr = new XMLHttpRequest();
+
+        xhr.open( 'POST', this.url, true );
+        xhr.responseType = 'json';
+    }
+
+    // Initializes XMLHttpRequest listeners.
+    _initListeners( resolve, reject ) {
+        const xhr = this.xhr;
+        const loader = this.loader;
+        const genericErrorText = 'Couldn\'t upload file:' + ` ${ loader.file.name }.`;
+
+        xhr.addEventListener( 'error', () => reject( genericErrorText ) );
+        xhr.addEventListener( 'abort', () => reject() );
+        xhr.addEventListener( 'load', () => {
+            const response = xhr.response;
+
+            if ( !response || response.error ) {
+                return reject( response && response.error ? response.error.message : genericErrorText );
+            }
+
+            // If the upload is successful, resolve the upload promise with an object containing
+            // at least the "default" URL, pointing to the image on the server.
+            resolve( {
+                default: response.url
+            } );
+        } );
+
+        if ( xhr.upload ) {
+            xhr.upload.addEventListener( 'progress', evt => {
+                if ( evt.lengthComputable ) {
+                    loader.uploadTotal = evt.total;
+                    loader.uploaded = evt.loaded;
+                }
+            } );
+        }
+    }
+
+    // Prepares the data and sends the request.
+    _sendRequest() {
+        const data = new FormData();
+
+        data.append( 'upload', this.loader.file );
+
+        this.xhr.send( data );
+    }
+}
