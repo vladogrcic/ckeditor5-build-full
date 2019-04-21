f4ca3f243943688ece5d512f0f298dbda2ba7ef4
vladogrcic <vladogrcic1993@gmail.com>
2019-04-21 03:14:07 +0200

    Adding the ability to add images using a custom interface.

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,7 +7,7 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
@@ -38,12 +38,19 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import ImageEmbed from './plugins/imageEmbed.js';
-export default class FullEditor extends ClassicEditorBase {}
+// import UploadAdapter from './plugins/uploadAdapter.js';
 
+export default class FullEditor extends ClassicEditorBase {}
+// var MyUploadAdapterPlugin = function ( editor ) {
+//     editor.plugins.get( 'FileRepository' ).createUploadAdapter = function( loader ) {
+//         // ...
+//     };
+// }
 // Plugins to include in the build.
+// FullEditor.extraPlugins = [ MyUploadAdapterPlugin ];
 FullEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
+	// UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
--- src/plugins/imageEmbed.js
+++ src/plugins/imageEmbed.js
@@ -6,7 +6,10 @@ import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 export default class ImageEmbed extends Plugin {
     init() {
         const editor = this.editor;
-
+		if (typeof embedImageExecuted != "undefined") {
+			embedImageExecuted(false);
+		}
+		window.executeEmbedImage = {};
         editor.ui.componentFactory.add( 'imageEmbed', locale => {
             const view = new ButtonView( locale );
 
@@ -18,16 +21,32 @@ export default class ImageEmbed extends Plugin {
 
             // Callback executed once the image is clicked.
             view.on( 'execute', () => {
-                const imageUrl = prompt( 'Image URL' );
-				if(imageUrl){
-					editor.model.change( writer => {
-						const imageElement = writer.createElement( 'image', {
-							src: imageUrl
-						} );
+				if (typeof embedImageExecuted != "undefined") {
+					embedImageExecuted(true);
+					executeEmbedImage = function (imageUrl) {
+						editor.model.change( writer => {
+							const imageElement = writer.createElement( 'image', {
+								src: imageUrl
+							} );
 
-						// Insert the image in the current selection location.
-						editor.model.insertContent( imageElement, editor.model.document.selection );
-					} );
+							// Insert the image in the current selection location.
+							editor.model.insertContent( imageElement, editor.model.document.selection );
+						});
+					}
+				}
+				else{
+					const imageUrl = prompt( 'Image URL' );
+					if(imageUrl){
+						editor.model.change( writer => {
+							const imageElement = writer.createElement( 'image', {
+								src: imageUrl
+							} );
+
+							// Insert the image in the current selection location.
+							editor.model.insertContent( imageElement, editor.model.document.selection );
+						});
+					}
+
 				}
             } );
 
